465f09f5a3af1d2b900eb8cd8249089a88c49dc0
Vaidas Bagdonas <vaidas.bagdonas@comsave.com>
2020-07-03 15:12:09 +0200

    add ModelSerializerTest

---
+++ tests/Unit/Comsave/SafeSalesforceSaverBundle/Services/ModelSerializerTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Tests\Unit\Comsave\SafeSalesforceSaverBundle\Services;
+
+use Comsave\SafeSalesforceSaverBundle\Services\ModelSerializer;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class ModelSerializerTest
+ * @package Tests\Unit\Comsave\SafeSalesforceSaverBundle\Services
+ * @coversDefaultClass \Comsave\SafeSalesforceSaverBundle\Services\ModelSerializer
+ */
+class ModelSerializerTest extends TestCase
+{
+    /** @var ModelSerializer */
+    private $modelSerializer;
+
+    public function setUp(): void
+    {
+        $this->modelSerializer = new ModelSerializer();
+    }
+
+    /**
+     * @covers ::serialize
+     */
+    public function testSerializeSingleModel(): void
+    {
+        $this->assertEquals('a:1:{i:0;O:8:"stdClass":0:{}}', $this->modelSerializer->serialize(new \stdClass()));
+    }
+
+    /**
+     * @covers ::serialize
+     */
+    public function testSerializeMultipleModel(): void
+    {
+        $this->assertEquals('a:2:{i:0;O:8:"stdClass":0:{}i:1;O:8:"stdClass":0:{}}', $this->modelSerializer->serialize([
+            new \stdClass(),
+            new \stdClass()
+        ]));
+    }
+
+    /**
+     * @covers ::serialize
+     */
+    public function testUnserializeSingleModel(): void
+    {
+        $this->assertEquals([
+            new \stdClass()
+        ], $this->modelSerializer->unserialize('a:1:{i:0;O:8:"stdClass":0:{}}'));
+    }
+
+    /**
+     * @covers ::serialize
+     */
+    public function testUnserializeMultipleModel(): void
+    {
+        $this->assertEquals([
+            new \stdClass(),
+            new \stdClass()
+        ], $this->modelSerializer->unserialize('a:2:{i:0;O:8:"stdClass":0:{}i:1;O:8:"stdClass":0:{}}'));
+    }
+}
